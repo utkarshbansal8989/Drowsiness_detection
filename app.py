40d1e2f752eedac9269f22bce549bd6531313dc5
Utkarsh Bansal <utkarshbansal0203@gmail.com>
2021-06-02 15:18:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -175,3 +175,6 @@ def video_feed():
     """Video streaming route. Put this in the src attribute of an img tag."""
     return Response(gen(),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == "__main__":
+    app.run(debug=True)
